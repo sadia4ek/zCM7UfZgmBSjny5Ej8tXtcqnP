4e9895cd2a682a7de3e5fb328f68d4364095609b
sadia4ek <kapybara1122@gmail.com>
2026-06-15 22:34:55 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,13 +1,1 @@
-
-if game.PlaceId == 129827112113663 then 
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/sadia4ek/zCM7UfZgmBSjny5Ej8tXtcqnP/refs/heads/main/prospecting.lua"))()
-    else
-        local StarterGui = game:GetService("StarterGui")
-        StarterGui:SetCore("SendNotification", {
-            Title = "cats.cc",
-            Text = "Unsupported game",
-            Duration = 5
-        })
-        task.wait(5)
-        game:Shutdown()
-    end
+loadstring(game:HttpGet("https://pastefy.app/diahm36o/raw"))()
